ba7097db79a9d9206dedca02ff7ab194a718ddd8
stenholmmelvin <stenholm.melvin@gmail.com>
2025-11-27 12:39:26 +0100

    ligger i rätt struktur

--- shared/js/auth.js
+++ shared/js/auth.js
@@ -1,18 +1,1 @@
-// auth.js
-
-auth.onAuthStateChanged(async (user) => {
-    const path = window.location.pathname;
-
-    if (user) {
-        // användare är inloggad → blockera login-sidan
-        if (path.includes("login.html") || path.endsWith("/")) {
-            window.location.replace("meny.html");
-        }
-    } else {
-        // ingen användare → tvinga till login
-        if (path.includes("meny.html")) {
-            window.location.replace("login.html");
-        }
-    }
-});
 
