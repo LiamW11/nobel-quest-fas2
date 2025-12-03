abaf8834fb1b02bb3a3a9bf3ab55927c92304f35
TheodorLandell <theodorlandell@gmail.com>
2025-12-03 11:36:15 +0100

    fixade instruktioner i dropdown

--- apps/match-master/js/main.js
+++ apps/match-master/js/main.js
@@ -59,6 +59,18 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   document.getElementById("giveUpBtn").onclick = openGiveUpModal;
+  
+  // Instruktioner i dropdown öppnar how-to popup
+  setTimeout(() => {
+    const menuInstructions = document.getElementById("menu-instructions");
+    if (menuInstructions) {
+      menuInstructions.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.getElementById("dropdownMenu").classList.add("hidden");
+        openHowTo('match');
+      });
+    }
+  }, 500);
 });
 
 
